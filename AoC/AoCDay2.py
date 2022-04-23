f2e9ff27c1cd099170c7dd72901fcc3e06755dfb
xposea <cheyenne.pourkay@gmail.com>
2022-04-22 20:14:11 -0400

    Update AoCDay2.py

--- AoC/AoCDay2.py
+++ AoC/AoCDay2.py
@@ -1,10 +1,30 @@
-if __name__ == '__main__':
+def part_one() -> int:
     depth, pos = 0, 0
-    for line in open("InputFiles/Day2.txt", "r"):
-        if line.split(" ")[0] == "forward":
-            pos += int(line.split(" ")[1])
-        elif line.split(" ")[0] == "up":
-            depth -= int(line.split(" ")[1])
-        elif line.split(" ")[0] == "down":
-            depth += int(line.split(" ")[1])
-    print(depth * pos)
+    for line in open("/Users/dylan/Library/CloudStorage/Box-Box/My Box Notes/Python/InputFiles/Day2.txt", "r"):
+        text, num = line.split()
+        if text == "forward":
+            pos += int(num)
+        elif text == "up":
+            depth -= int(num)
+        elif text == "down":
+            depth += int(num)
+    return depth * pos
+
+
+def part_two() -> int:
+    depth, pos, aim = 0, 0, 0
+    for line in open("/Users/dylan/Library/CloudStorage/Box-Box/My Box Notes/Python/InputFiles/Day2.txt", "r"):
+        text, num = line.split()
+        if text == "forward":
+            pos += int(num)
+            depth += aim * int(num)
+        elif text == "up":
+            aim -= int(num)
+        elif text == "down":
+            aim += int(num)
+    return depth * pos
+
+
+if __name__ == '__main__':
+    print(part_one())
+    print(part_two())
